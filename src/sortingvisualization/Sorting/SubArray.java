0f478653d838964bbbb85eb7c01678dd408d9c35
klnmi97 <32402640+klnmi97@users.noreply.github.com>
2017-11-19 23:39:30 +0100

    final implementation of SubArray class

---
+++ src/sortingvisualization/Sorting/SubArray.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingvisualization.Sorting;
+
+import sortingvisualization.Core.AbstractSubArray;
+
+/**
+ *
+ * @author mihae
+ */
+public class SubArray extends AbstractSubArray {
+    
+    public SubArray(int size) {
+        super(size);
+    }
+    
+}
