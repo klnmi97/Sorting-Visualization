9a34112cb01c40bca4f6fb08b180e5892b5b8e96
klnmi97 <mishaklunko97@gmail.com>
2019-03-20 22:09:34 +0100

    Added constants class

---
+++ src/sortingvisualization/Constants/Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingvisualization.Constants;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author mihae
+ */
+public class Constants {
+    
+    public static final Color PANEL_BGND = /*Color.valueOf("#F0C243")*/Color.AQUAMARINE;
+    public static final Color LOW_PAN_BGND = Color.LIGHTPINK;
+    //colours of bricks:
+    //COMPARE - selected, DEFAULT - main color
+    public static final Color COMPARE = /*Color.valueOf("#F2355B");*/Color.AQUAMARINE;
+    public static final Color DEFAULT = /*Color.valueOf("#02A694");*/Color.DARKCYAN;
+    public static final Color SORTED = /*Color.valueOf("#F38902");//*/Color.ORANGE;
+    public static final Color LINE_SELECTION = Color.WHITE;
+}
