7e2619b1968df26bdd70cc0a9df904ec6bbc67c3
klnmi97 <michael.klunko@gmail.com>
2019-03-27 16:27:02 +0100

    InfoDialog class

---
+++ src/sortingvisualization/UI/InfoDialog.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingvisualization.UI;
+
+/**
+ *
+ * @author Mykhailo Klunko
+ */
+public class InfoDialog {
+    private String message = "This is short description";
+    
+    
+}
