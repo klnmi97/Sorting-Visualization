42c0d8fc42f808387b80ea7a064e3ad812e7e095
klnmi97 <michael.klunko@gmail.com>
2019-04-12 08:53:00 +0200

    #16 - color info enhancements

--- src/sortingvisualization/NodeControllers/ColorInfoManager.java
+++ src/sortingvisualization/NodeControllers/ColorInfoManager.java
@@ -8,7 +8,9 @@ package sortingvisualization.NodeControllers;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Pair;
@@ -40,7 +42,10 @@ public class ColorInfoManager {
         }
         
         box.setSpacing(10 * scalingFactor);
-        return box;
+        
+        Line divider = new Line(0, 0, 500, 0);
+        VBox colorInfo = new VBox(divider, box);
+        return colorInfo;
     }
     
 }
